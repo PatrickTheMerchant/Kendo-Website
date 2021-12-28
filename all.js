87c12f945d6f2c2d5529fa88aa34230ad6b245b3
PatrickTheMerchant <71906962+PatrickTheMerchant@users.noreply.github.com>
2021-12-28 03:36:38 -0600

    Add files via upload

---
+++ all.js
@@ -0,0 +1,12 @@
+// Set title of page
+var clubinfo = " | Cornell Kendo Club";
+var path = window.location.pathname;
+var pagename = path.substring(path.lastIndexOf("/") + 1)
+if (pagename == "kendo") pagename = "home";
+else if (pagename == "eBoard") pagename = "E-Board";
+pagename = pagename.toUpperCase().substr(0, 1) + pagename.substring(1);
+document.title = pagename + clubinfo;
+
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
